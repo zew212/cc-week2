1b680df202601d244f62c95e5f9b18a91fa79258
zew212 <zew212@nyu.edu>
2026-09-11 12:23:32 -0400

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,19 @@
+console.log("this is working");
+
+
+// target pink+black
+const Lace = document.querySelector("#Lace");
+
+// move to random position
+
+// create a radom variable and assign it to a random variable
+
+
+let randomWidth= Math.random() * 400;
+
+let widthString = randomWidth + "px"
+
+console.log(widthString)
+
+silver.style.top = widthString;
+
